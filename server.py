cec23d8ea323fe7113d9259607e01afaf71642a5
Ahsen Jaffer <ahsen.jaffer@gmail.com>
2025-05-27 18:43:36 -0700

    Add screenshot

--- server.py
+++ server.py
@@ -90,5 +90,5 @@ def logs_json():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5003))
     app.run(host="0.0.0.0", port=port)
