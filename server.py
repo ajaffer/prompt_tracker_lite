bc67d3956f2e14b527c40062a652aeaafbcf0d81
Ahsen Jaffer <ahsen.jaffer@gmail.com>
2025-05-27 18:44:50 -0700

    [minor] update server port

--- server.py
+++ server.py
@@ -90,5 +90,5 @@ def logs_json():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5003))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
